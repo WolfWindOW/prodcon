49a0a9eff4d63dfc88bb66ab97891e0047905763
WolfWindOW <55042320+WolfWindOW@users.noreply.github.com>
2021-10-25 17:23:56 -0400

    Adding comments to consumer and producer

--- consumer.c
+++ consumer.c
@@ -12,9 +12,13 @@
 
 int main() {
 
+    //Shm_fd == file descriptor of shared memory space
+    //"table" is the name of the shared memory space
+    //table is the size of int, which is 2 in this instance
     int shm_fd = shm_open("table", O_RDWR, 0666);
     int *table = mmap(0, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
+    //Creates three semaphores, fill, available and mutex
     sem_t *fill = sem_open("fill", O_CREAT, 0666, 0);
     sem_t *avail = sem_open("available", O_CREAT, 0666, 2);
     sem_t *mutex = sem_open("mutex", O_CREAT, 0666, 1);
@@ -23,32 +27,40 @@ int main() {
     printf("\nConsumer ready to receive %d items.\n", loop);
 
     for(int i = 0; i < loop; ++i) {
+        //Waits for the fill flag
         sem_wait(fill);
 
+        //Sleeps for anywhere from 1 to 4 seconds
         int ms = rand() % 4 + 1;
         sleep(ms);
 
+        //Waits until the shared memory isnt in use, decrements it, then releases it
         sem_wait(mutex);
         --(*table);
         sem_post(mutex);
 
         printf("Item consumed, %d remaining.\n", *table);
 
+        //Posts that there is space available in the shared memory
         sem_post(avail);
     }
 
+    //Closes access to the semaphores
     sem_close(fill);
     sem_close(avail);
     sem_close(mutex);
 
+    //Unlinks the semaphores
     sem_unlink("fill");
     sem_unlink("available");
     sem_unlink("mutex");
 
+    //Releases the semaphores
     sem_destroy(fill);
     sem_destroy(avail);
     sem_destroy(mutex);
 
+    //Releases and unlinks the shared memory
     munmap(table, sizeof(int));
     shm_unlink("table");
 
--- producer.c
+++ producer.c
@@ -17,9 +17,8 @@ int main() {
 
     //tbl == file descriptor
     //table == base address
-    //int *ptr, movable
-
     int tbl = shm_open("table", O_CREAT | O_RDWR, 0666);
+    //Errors if unable to create a shared memory space
     if(tbl == -1) {
         printf("Shared memory failed: %s\n", strerror(errno));
         exit(1);
@@ -28,11 +27,13 @@ int main() {
     ftruncate(tbl,sizeof(int));
     //Mapping shared memory segment to address of the process
     int *table = mmap(0, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, tbl, 0);
+    //Errors if the segment isnt able to be mapped
     if(table == MAP_FAILED) {
         printf("Map failed: %s\n", strerror(errno));
         exit(1);
     }
 
+    //Creates three semaphores, fill, available, and mutex
     sem_t *fill = sem_open("fill", O_CREAT, 0666, 0);
     sem_t *avail = sem_open("available", O_CREAT, 0666, 2);
     sem_t *mutex = sem_open("mutex", O_CREAT, 0666, 1);
@@ -40,31 +41,40 @@ int main() {
     int loop = 10;
     printf("\nProducer ready to create %d items. \n", loop);
     for(int i=0; i < loop; i++) {
+        //Waits until the memory space has space available for manipulation
         sem_wait(avail);
-        //printf("1");
+
+        //Sleeps 1 second
         int ms = rand() % 2 + 1;
         sleep(ms);
-        //printf("2");
+
+        //Waits until the memory segment is not in use, then increments, and releases
         sem_wait(mutex);
         ++(*table);
         sem_post(mutex);
 
         printf("Item produced, there are now %d item(s) in the table.\n", *table);
+        //Sends signal that the table has an item to be consumed
         sem_post(fill);
     }
 
+    //Closes access
     sem_close(fill);
     sem_close(avail);
     sem_close(mutex);
 
+    //Unlinks semaphores
     sem_unlink("fill");
     sem_unlink("available");
     sem_unlink("mutex");
 
+    //Releases resources
     sem_destroy(fill);
     sem_destroy(avail);
     sem_destroy(mutex);
 
+    //Unmaps the table and unlinks.
+    //Closes the table **unique to producer
     munmap(table, sizeof(int));
     close(tbl);
     shm_unlink("table");
