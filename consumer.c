2b042a118ee0e7bb0edbc329a93b137650ba3777
WoahOW <55042320+WolfWindOW@users.noreply.github.com>
2021-10-26 11:54:04 -0400

    Refactor

--- consumer.c
+++ consumer.c
@@ -18,17 +18,17 @@ int main() {
     int shm_fd = shm_open("table", O_RDWR, 0666);
     int *table = mmap(0, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
-    //Creates three semaphores, fill, available and mutex
-    sem_t *fill = sem_open("fill", O_CREAT, 0666, 0);
-    sem_t *avail = sem_open("available", O_CREAT, 0666, 2);
+    //Creates three semaphores, data, empty and mutex
+    sem_t *data = sem_open("data", O_CREAT, 0666, 0);
+    sem_t *empty = sem_open("empty", O_CREAT, 0666, 2);
     sem_t *mutex = sem_open("mutex", O_CREAT, 0666, 1);
 
     int loop = 10;
     printf("\nConsumer ready to receive %d items.\n", loop);
 
     for(int i = 0; i < loop; ++i) {
-        //Waits for the fill flag
-        sem_wait(fill);
+        //Waits for the data flag
+        sem_wait(data);
 
         //Sleeps for anywhere from 1 to 4 seconds
         int ms = rand() % 4 + 1;
@@ -42,22 +42,22 @@ int main() {
         printf("Item consumed, %d remaining.\n", *table);
 
         //Posts that there is space available in the shared memory
-        sem_post(avail);
+        sem_post(empty);
     }
 
     //Closes access to the semaphores
-    sem_close(fill);
-    sem_close(avail);
+    sem_close(data);
+    sem_close(empty);
     sem_close(mutex);
 
     //Unlinks the semaphores
-    sem_unlink("fill");
-    sem_unlink("available");
+    sem_unlink("data");
+    sem_unlink("empty");
     sem_unlink("mutex");
 
     //Releases the semaphores
-    sem_destroy(fill);
-    sem_destroy(avail);
+    sem_destroy(data);
+    sem_destroy(empty);
     sem_destroy(mutex);
 
     //Releases and unlinks the shared memory
@@ -65,6 +65,6 @@ int main() {
     shm_unlink("table");
 
     printf("Consumer cleaned up.\n");
-    exit(1);
+    //exit(1);
     return 0;
 }
